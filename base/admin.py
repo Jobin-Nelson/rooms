3c56237aa5a8657ea4721d4fc3b2fe45e430f413
Jobin Nelson <jobinnelson369@gmail.com>
2023-06-17 16:33:13 +0530

    add login page

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, Topic, Message
 
 # Register your models here.
-admin.site.register(Room)
+
+custom_models = [
+    Room,
+    Topic,
+    Message,
+]
+
+for model in custom_models:
+    admin.site.register(model)
