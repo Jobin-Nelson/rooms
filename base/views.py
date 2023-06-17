2486de49252961579af76218d516fd4daed92a30
Jobin Nelson <jobinnelson369@gmail.com>
2023-06-17 14:55:12 +0530

    add Room Topic Message models

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Room
 
 rooms = [
     {'id': 1, 'name': 'Lets learn python!'},
@@ -7,16 +8,14 @@ rooms = [
 ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {
         'rooms': rooms,
     }
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {
         'room': room,
     }
